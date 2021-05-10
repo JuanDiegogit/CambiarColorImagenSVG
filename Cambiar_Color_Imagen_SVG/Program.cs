3614a4c87be9da81414678efe7008e8a9c425f6a
Juan Diego Mejía Maestre <Mejiamaestrejuandiego@gmail.com>
2021-05-10 17:35:41 -0500

    Agregar archivos de proyecto.

---
+++ Cambiar_Color_Imagen_SVG/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cambiar_Color_Imagen_SVG
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
